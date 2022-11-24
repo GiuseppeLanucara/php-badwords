bb2db2cab428dbb2d3a10de2149a86387347f1af
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-24 16:46:02 +0100

    aggiunto style alle pagine

--- inter.php
+++ inter.php
@@ -9,13 +9,14 @@ $usercensored = $_GET["usercensored"]
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>PHP-Hello</title>
 </head>
 <body>
     <h1><?php echo $userpara ?></h1>
     <h4> Il paragrafo che hai scritto ha  <?php echo  strlen($userpara); ?> parole. </h4>
     <hr>
-    <h1>Ecco il paragrafo censurato</h1>
-    <h4><?php echo str_replace($usercensored, "***", $userpara); ?></h4>
+    <h4>Ecco il paragrafo censurato</h4>
+    <h1><?php echo str_replace($usercensored, "***", $userpara); ?></h1>
 </body>
 </html>
