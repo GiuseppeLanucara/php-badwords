37a0d9c1ff152e61d0a5bf77a9b97c528c9b47b4
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-24 15:52:27 +0100

    creato collegamento e stampa paragrafo

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP-Hello</title>
 </head>
 <body>
-    
+    <form action="inter.php" method="GET">
+        <label for="paragrafo">Scrivi un paragrafo</label>
+        <textarea name="userpara" id="paragrafo" cols="30" rows="10"></textarea>
+        <hr>
+    <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
